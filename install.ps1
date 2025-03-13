a8f97dc919b6790c700ef1809049d18bc7538055
Неолайн <135314083+iineolineii@users.noreply.github.com>
2025-03-14 01:18:09 +0300

    Fix script downloading

--- install.ps1
+++ install.ps1
@@ -1,3 +1,9 @@
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser;
-mkdir -f -p $PROFILE\..;
-echo (curl https://github.com/iineolineii/ssh-copy-id.ps1/raw/refs/heads/main/ssh-copy-id.ps1).Content >> $PROFILE;
+# Check if $PROFILE's parent directory exists
+$profileDir = Split-Path -Path $PROFILE
+if (!(Test-Path $profileDir)) {
+    New-Item -ItemType Directory -Path $profileDir -Force
+}
+
+# Download the script and append it to the $PROFILE
+$profileContent = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/iineolineii/ssh-copy-id.ps1/main/ssh-copy-id.ps1").Content
+$profileContent | Out-File -Append -Encoding utf8 $PROFILE
